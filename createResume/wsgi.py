5be0104ae7eae282c35f34e3eb3f4d905706413e
guoxudong <88137956@qq.com>
2018-03-05 10:03:05 +0800

    新增 uwsgi

--- createResume/wsgi.py
+++ createResume/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "createResume.settings")
 
 application = get_wsgi_application()
+sys.path.append("/apps/CreateTesume")
